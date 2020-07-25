35b0cfd9c52d85aa3245005f179df27984564931
rafael romao <slipknot55566618@hotmail.com>
2020-07-24 23:20:15 -0300

    V1.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,37 +12,49 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            bool backTop = true;
 
-            Console.Write("Enter Operator: ");
-            string op = Console.ReadLine();
+            while (backTop)
 
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            {
+
+                Console.Write("Enter a number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Operator: ");
+                string op = Console.ReadLine();
+
+                Console.Write("Enter a number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
 
 
 
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
+                if (op == "+")
+                {
+                    Console.WriteLine(num1 + num2);
+                }
+                else if (op == "-")
+                {
+                    Console.WriteLine(num1 - num2);
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+                else if (op == "*")
+                {
+                    Console.WriteLine("The result is: " + num1 * num2);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Operator Try Again");
+                }
+
+                Console.WriteLine();
+
             }
-            else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine("The result is: " + num1 * num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Operator Try Again");
-            }
+
+            
 
             Console.ReadLine();
 
